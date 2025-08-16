e4b486fd6eb8141a7a77e8ba4b601e6d27e5a84f
Saurabh <saurabh@Saurabhs-MacBook-Air.local>
2025-08-16 16:56:25 +0530

    routing changed Admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import { Admin, Resource } from "react-admin";
+import { BrowserRouter } from "react-router-dom";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 import BlogCreate from "./pages/blogs/BlogCreate";
 import BlogList from "./pages/blogs/BlogList";
 import BlogShow from "./pages/blogs/BlogShow";
-import BlogEdit  from "./pages/blogs/BlogEdit";
+import BlogEdit from "./pages/blogs/BlogEdit";
 
+const basename = import.meta.env.PROD ? "/admin" : "/";
 
 export const App = () => (
-  <Admin
-    basename={import.meta.env.PROD ? "/admin" : undefined}
-    layout={Layout}
-    dataProvider={dataProvider}
-    authProvider={authProvider}
-  >
-     <Resource name="blogs" list={BlogList} create={BlogCreate} show={BlogShow} edit={BlogEdit}/>
- 
-  </Admin>
+  <BrowserRouter basename={basename}>
+    <Admin
+      layout={Layout}
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
+      <Resource
+        name="blogs"
+        list={BlogList}
+        create={BlogCreate}
+        show={BlogShow}
+        edit={BlogEdit}
+      />
+    </Admin>
+  </BrowserRouter>
 );
